8cc0d93aa6d5e452746817be047c03e0b90207f4
Calum Leslie <calumleslie@gmail.com>
2016-01-30 22:11:02 +0000

    Implement prefixes "properly".
    Not sure how you're meant to tell ones which have just a nickname and
    ones which have just a servername apart. It is late and I may be
    missing something.

--- src/lib.rs
+++ src/lib.rs
@@ -7,42 +7,107 @@ use std::vec::Vec;
 
 // Maybe consider storing the whole line and references?
 #[derive(Debug,Clone)]
-pub struct IrcLine<'a> {
-    prefix: Option<&'a str>,
+pub struct Message<'a> {
+    prefix: Prefix<'a>,
     command: &'a str,
     arguments: Vec<&'a str>,
 }
 
-impl<'a> Display for IrcLine<'a> {
-    fn fmt(&self, fmt: &mut Formatter) -> std::fmt::Result {
-    	try!(match self.prefix {
-    		None => Ok( () ),
-    		Some(value) => write!(fmt, ":{} ", value )
-    	});
+#[derive(Debug,Clone)]
+pub enum Prefix<'a> {
+    None,
+    Server(&'a str),
+    User(UserInfo<'a>),
+}
 
-        try!( write!(fmt, "{}", self.command) );
+#[derive(Debug,Clone)]
+pub struct UserInfo<'a> {
+    nickname: &'a str,
+    host: Option<HostInfo<'a>>,
+}
+
+#[derive(Debug,Clone)]
+pub struct HostInfo<'a> {
+    user: Option<&'a str>,
+    host: &'a str,
+}
+
+impl<'a> UserInfo<'a> {
+    pub fn nickname(nickname: &'a str) -> Self {
+        UserInfo {
+            nickname: nickname,
+            host: None,
+        }
+    }
+
+    pub fn nickname_host(nickname: &'a str, host: &'a str) -> Self {
+        UserInfo {
+            nickname: nickname,
+            host: Some(HostInfo {
+                host: host,
+                user: None,
+            }),
+        }
+    }
+
+    pub fn nickname_user_host(nickname: &'a str, user: &'a str, host: &'a str) -> Self {
+        UserInfo {
+            nickname: nickname,
+            host: Some(HostInfo {
+                host: host,
+                user: Some(user),
+            }),
+        }
+    }
+}
+
+impl<'a> Display for UserInfo<'a> {
+    fn fmt(&self, fmt: &mut Formatter) -> std::fmt::Result {
+        try!(write!(fmt, "{}", self.nickname));
+
+        match self.host {
+            None => Ok(()),
+            Some(ref host_info) => {
+                match host_info.user {
+                    Some(user) => write!(fmt, "!{}@{}", user, host_info.host),
+                    None => write!(fmt, "@{}", host_info.host),
+                }
+            }
+        }
+    }
+}
+
+impl<'a> Display for Message<'a> {
+    fn fmt(&self, fmt: &mut Formatter) -> std::fmt::Result {
+        try!(match self.prefix {
+            Prefix::None => Ok(()),
+            Prefix::Server(ref server) => write!(fmt, ":{} ", server),
+            Prefix::User(ref user_info) => write!(fmt, ":{} ", user_info),
+        });
+
+        try!(write!(fmt, "{}", self.command));
 
         for (i, argument) in self.arguments.iter().enumerate() {
-        	try!( write!( fmt, " " ) );
+            try!(write!(fmt, " "));
 
-        	if i == self.arguments.len() - 1 && argument.contains( ' ' ) {
-        		try!( write!( fmt, ":" ) );
-        	}
+            if i == self.arguments.len() - 1 && argument.contains(' ') {
+                try!(write!(fmt, ":"));
+            }
 
-        	try!( write!( fmt, "{}", argument ) );
+            try!(write!(fmt, "{}", argument));
         }
 
-        Ok( () )
+        Ok(())
     }
 }
 
 #[cfg(test)]
 mod tests {
-	use super::*;
+    use super::*;
     #[test]
     fn command_only() {
-        let line = IrcLine {
-            prefix: None,
+        let line = Message {
+            prefix: Prefix::None,
             command: "PING",
             arguments: vec![],
         };
@@ -51,46 +116,84 @@ mod tests {
     }
 
     #[test]
-    fn prefix_command() {
-    	let line = IrcLine {
-    		prefix: Some( "somedude" ),
-    		command: "PING",
-    		arguments: vec![],
-    	};
+    fn server_prefix() {
+        let line = Message {
+            prefix: Prefix::Server("somedude"),
+            command: "PING",
+            arguments: vec![],
+        };
 
-    	assert_eq!(format!("{}", line), ":somedude PING" );
+        assert_eq!(format!("{}", line), ":somedude PING");
+    }
+
+    #[test]
+    fn user_prefix_nickname_only() {
+        let line = Message {
+            prefix: Prefix::User(UserInfo::nickname("nickname")),
+            command: "PING",
+            arguments: vec![],
+        };
+
+        assert_eq!(format!("{}", line), ":nickname PING");
+    }
+
+    #[test]
+    fn user_prefix_nickname_host() {
+        let line = Message {
+            prefix: Prefix::User(UserInfo::nickname_host("nickname", "some.host.name")),
+            command: "PING",
+            arguments: vec![],
+        };
+
+        assert_eq!(format!("{}", line), ":nickname@some.host.name PING");
+    }
+
+    #[test]
+    fn user_prefix_all_user_info() {
+        let line = Message {
+            prefix: Prefix::User(UserInfo::nickname_user_host("nickname",
+                                                              "realname",
+                                                              "some.host.name")),
+            command: "PING",
+            arguments: vec![],
+        };
+
+        assert_eq!(format!("{}", line),
+                   ":nickname!realname@some.host.name PING");
     }
 
     #[test]
     fn command_args() {
-    	let line = IrcLine {
-    		prefix: None,
-    		command: "PRIVMSG",
-    		arguments: vec![ "someone", "something" ],
-    	};
+        let line = Message {
+            prefix: Prefix::None,
+            command: "PRIVMSG",
+            arguments: vec!["someone", "something"],
+        };
 
-    	assert_eq!(format!("{}", line), "PRIVMSG someone something" );
+        assert_eq!(format!("{}", line), "PRIVMSG someone something");
     }
 
     #[test]
     fn command_args_with_long_final_argument() {
-    	let line = IrcLine {
-    		prefix: None,
-    		command: "PRIVMSG",
-    		arguments: vec![ "someone", "Hey I love being on IRC" ],
-    	};
+        let line = Message {
+            prefix: Prefix::None,
+            command: "PRIVMSG",
+            arguments: vec!["someone", "Hey I love being on IRC"],
+        };
 
-    	assert_eq!(format!("{}", line), "PRIVMSG someone :Hey I love being on IRC" );
+        assert_eq!(format!("{}", line),
+                   "PRIVMSG someone :Hey I love being on IRC");
     }
 
     #[test]
     fn everything() {
-    	let line = IrcLine {
-    		prefix: Some("information"),
-    		command: "PRIVMSG",
-    		arguments: vec![ "someone", "something", "Hey I love being on IRC" ],
-    	};
+        let line = Message {
+            prefix: Prefix::Server("information"),
+            command: "PRIVMSG",
+            arguments: vec!["someone", "something", "Hey I love being on IRC"],
+        };
 
-    	assert_eq!(format!("{}", line), ":information PRIVMSG someone something :Hey I love being on IRC" );
+        assert_eq!(format!("{}", line),
+                   ":information PRIVMSG someone something :Hey I love being on IRC");
     }
 }
