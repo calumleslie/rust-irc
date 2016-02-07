1675b02812cd3ff38e868d0967f3c3e3cb6ae65b
Calum Leslie <calumleslie@gmail.com>
2016-02-07 12:40:21 +0000

    Replace parser::parse_message with Message::parse.
    Turns out this just works!

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ extern crate nom;
 
 mod command;
 mod message;
-pub mod parser;
+mod parser;
 
 pub use command::Command;
 pub use command::responses;
@@ -11,3 +11,23 @@ pub use command::commands;
 pub use message::Message;
 pub use message::Prefix;
 pub use message::UserInfo;
+
+use parser::parse_message;
+
+impl<'a> Message<'a> {
+    pub fn parse(input: &'a [u8]) -> Result<(Message, &'a [u8]), ()> {
+        parse_message(input)
+    }
+}
+
+#[test]
+fn simple_parse() {
+    match Message::parse("PING 12345\r\nsome other content".as_bytes()) {
+        Ok((msg, remaining)) => {
+            assert_eq!(msg,
+                       Message::new(Prefix::None, commands::PING, vec!["12345"]));
+            assert_eq!(remaining, "some other content".as_bytes());
+        }
+        other => panic!("{:?}", other),
+    }
+}
