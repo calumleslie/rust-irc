4048b889fca42c6696fd420e7c4621b01e9d8c70
Calum Leslie <calumleslie@gmail.com>
2016-02-07 12:13:45 +0000

    A more complete parser implementation.
    More forgiving than it should be and panics in some places untidily.

--- src/parser.rs
+++ src/parser.rs
@@ -1,11 +1,16 @@
 
 use std::str;
 use std::str::FromStr;
+use std::vec::Vec;
 use nom::IResult;
 use nom::GetInput;
+use nom::Err;
 use command::Command;
 use command::commands;
 use command::responses;
+use message::Message;
+use message::Prefix;
+use message::UserInfo;
 // message    =  [ ":" prefix SPACE ] command [ params ] crlf
 //     prefix     =  servername / ( nickname [ [ "!" user ] "@" host ] )
 //     command    =  1*letter / 3digit
@@ -57,22 +62,10 @@ fn is_letter(c: u8) -> bool {
     (c >= b'a' && c <= b'z') || (c >= b'A' && c <= b'Z')
 }
 
+// TODO Think nom has a builtin for this
 fn is_digit(c: u8) -> bool {
     c >= b'0' && c <= b'9'
 }
-
-fn make_word<'a>(input: &'a [u8]) -> Result<Command<'a>, str::Utf8Error> {
-    str::from_utf8(input).map(|w| Command::Word(w))
-}
-
-fn make_number<'a>(input: &'a [u8]) -> Result<Command<'a>, str::Utf8Error> {
-    str::from_utf8(input).map(|text| u16::from_str(text).unwrap()).map(|n| Command::Number(n))
-}
-
-named!(command<Command>, alt!( word_command | numeric_command ) );
-named!(word_command<Command>, map_res!( take_while1!(is_letter), make_word) );
-// TODO: This does not limit values to 3 digits, and no validation in make_number.
-named!(numeric_command<Command>, map_res!( take_while!(is_digit), make_number ) );
 
 // This is a horrible hack; just over-match and allow anything
 // that can be in an IPv4 address, IPv6 address, or the RFC's
@@ -82,6 +75,94 @@ fn is_host_char(c: u8) -> bool {
     is_letter(c) || is_digit(c) || c == b'.' || c == b':'
 }
 
+// Everything except NUL, CR, LF, " " and ":"
+fn nospcrlfcl(c: u8) -> bool {
+    (c != 0) && (c != b'\r') && (c != b'\n') && (c != b' ') && (c != b':')
+}
+
+// "[", "]", "\", "`", "_", "^", "{", "|", "}"
+fn is_special(c: u8) -> bool {
+    (c == b'[') || (c == b']') || (c == b'\\') || (c == b'`') || (c == b'_') || (c == b'^') ||
+    (c == b'{') || (c == b'|') || (c == b'}')
+}
+
+fn trailing_char(c: u8) -> bool {
+    (c == b' ') || (c == b':') || nospcrlfcl(c)
+}
+
+fn make_word<'a>(input: &'a [u8]) -> Result<Command<'a>, str::Utf8Error> {
+    str::from_utf8(input).map(|w| Command::Word(w))
+}
+
+fn make_number<'a>(input: &'a [u8]) -> Result<Command<'a>, str::Utf8Error> {
+    str::from_utf8(input).map(|text| u16::from_str(text).unwrap_or(123)).map(|n| Command::Number(n))
+}
+
+fn make_user_prefix<'a>(user_info: UserInfo<'a>) -> Prefix<'a> {
+    Prefix::User(user_info)
+}
+
+fn make_server_prefix<'a>(input: &'a str) -> Prefix<'a> {
+    Prefix::Server(input)
+}
+
+named!(message<Message>, chain!(
+  prefix: prefix? ~
+  command: command ~
+  params: params, ||{
+    Message::new( prefix.unwrap_or( Prefix::None ), command, params ) 
+  }
+));
+
+named!(params<Vec<&str> >, many0!( preceded!( tag!(" "), alt!( param | final_param ) ) ) );
+named!(param<&str>, map_res!( take_while1!(nospcrlfcl), str::from_utf8 ) );
+named!(final_param<&str>, preceded!( tag!(":"), trailing ) );
+named!(trailing<&str>, map_res!( take_while!(trailing_char), str::from_utf8 ) );
+
+named!(command<Command>, alt!( word_command | numeric_command ) );
+named!(word_command<Command>, map_res!( take_while1!(is_letter), make_word) );
+// TODO: This does not limit values to 3 digits, and no validation in make_number.
+named!(numeric_command<Command>, map_res!( take_while1!(is_digit), make_number ) );
+
+
+// ( nickname [ [ "!" user ] "@" host ] )
+
+//   letter     =  %x41-5A / %x61-7A       ; A-Z / a-z
+//   digit      =  %x30-39                 ; 0-9
+//   hexdigit   =  digit / "A" / "B" / "C" / "D" / "E" / "F"
+//   special    =  %x5B-60 / %x7B-7D
+//                    ; "[", "]", "\", "`", "_", "^", "{", "|", "}"
+
+fn is_nickname_char(c: u8) -> bool {
+    is_letter(c) || is_special(c) || is_digit(c) || c == b'-'
+}
+
+// Not NUL, CR, LF, " " and "@"
+fn is_username_char(c: u8) -> bool {
+    (c != 0) && (c != b'\r') && (c != b'\n') && (c != b' ') && (c != b'@')
+}
+
+// This consumes the final space too, a simple way of testing we eat everything
+// up to the delimiter.
+named!(prefix<Prefix>, preceded!( tag!( ":" ), alt!( 
+  complete!( terminated!( user_prefix, tag!( " " ) ) )
+| complete!( terminated!( server_prefix, tag!( " " ) ) ) ) ) );
+
+named!(user_prefix<Prefix>, map!(user_info, make_user_prefix ) );
+named!(server_prefix<Prefix>, dbg!( map!( host, make_server_prefix ) ) );
+
+// Use of complete! here stops the earlier patterns returning Incomplete.
+named!(user_info<UserInfo>, alt!(
+  complete!( chain!( n: nickname ~ tag!("!") ~ u: username ~ tag!("@") ~ h: host, ||{
+    UserInfo::of_nickname_user_host( n, u, h )
+  } ) )
+| complete!( chain!( n: nickname ~ tag!("@") ~ h: host, ||{ UserInfo::of_nickname_host( n, h ) } ) )
+| map!( nickname, |value|{ UserInfo::of_nickname( value ) } )
+));
+
+// Note: This allows nicknames with invalid first characters
+named!(nickname<&str>, map_res!( take_while1!(is_nickname_char), str::from_utf8));
+named!(username<&str>, map_res!( take_while1!(is_username_char), str::from_utf8));
 named!(host<&str>, map_res!( take_while1!(is_host_char), str::from_utf8));
 
 
@@ -139,6 +220,106 @@ fn command_word() {
 fn command_numeric() {
     match command("004".as_bytes()) {
         IResult::Done(_, out) => assert_eq!(out, responses::RPL_MYINFO),
+        other => panic!("{:?}", other),
+    }
+}
+
+#[test]
+fn final_param_with_content() {
+    match final_param(":content can contain spaces and ':'".as_bytes()) {
+        IResult::Done(_, out) => assert_eq!(out, "content can contain spaces and ':'"),
+        other => panic!("{:?}", other),
+    }
+}
+
+#[test]
+fn params_multiple() {
+    match params(" here are some :parameters including a long final one".as_bytes()) {
+        IResult::Done(_, out) => {
+            assert_eq!(out,
+                       vec!["here", "are", "some", "parameters including a long final one"])
+        }
+        other => panic!("{:?}", other),
+    }
+}
+
+#[test]
+fn params_no_trailing() {
+    match params(" here are some parameters".as_bytes()) {
+        IResult::Done(_, out) => assert_eq!(out, vec!["here", "are", "some", "parameters"]),
+        other => panic!("{:?}", other),
+    }
+}
+
+#[test]
+fn message_no_prefix() {
+    match message("PRIVMSG someone :Hey what is up".as_bytes()) {
+        IResult::Done(_, out) => {
+            assert_eq!(out,
+                       Message::new(Prefix::None,
+                                    commands::PRIVMSG,
+                                    vec!["someone", "Hey what is up"]))
+        }
+        other => panic!("{:?}", other),
+    }
+}
+
+#[test]
+fn message_user_prefix() {
+    match message(":x!y@z PRIVMSG someone :Hey what is up".as_bytes()) {
+        IResult::Done(_, out) => {
+            assert_eq!(out,
+                       Message::new(Prefix::User(UserInfo::of_nickname_user_host("x", "y", "z")),
+                                    commands::PRIVMSG,
+                                    vec!["someone", "Hey what is up"]))
+        }
+        other => panic!("{:?}", other),
+    }
+}
+
+#[test]
+fn message_server_prefix() {
+    match message(":some.where PRIVMSG someone :Hey what is up".as_bytes()) {
+        IResult::Done(_, out) => {
+            assert_eq!(out,
+                       Message::new(Prefix::Server("some.where"),
+                                    commands::PRIVMSG,
+                                    vec!["someone", "Hey what is up"]))
+        },
+        IResult::Error(Err::Position(_, r)) => panic!("{:?}", str::from_utf8( r ) ),
+        other => panic!("{:?}", other),
+    }
+}
+
+#[test]
+fn prefix_server() {
+    match prefix(":some.where.com ".as_bytes()) {
+        IResult::Done(_, out) => {
+            assert_eq!(out,
+                       Prefix::Server("some.where.com"))
+        }
+        other => panic!("{:?}", other),
+    }
+}
+
+#[test]
+fn prefix_user_prefix_full() {
+    match prefix(":x!y@z ".as_bytes()) {
+        IResult::Done(_, out) => {
+            assert_eq!(out,
+                       Prefix::User(UserInfo::of_nickname_user_host("x", "y", "z")))
+        }
+        other => panic!("{:?}", other),
+    }
+}
+
+#[test]
+fn prefix_user_prefix_nickname_only() {
+    match prefix(":aperson ".as_bytes()) {
+        IResult::Done(_, out) => {
+            assert_eq!(out,
+                       Prefix::User(UserInfo::of_nickname("aperson")))
+        }
         other => panic!("{:?}", other),
     }
 }
