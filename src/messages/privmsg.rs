e13af8c2391b1a18ceecb047e9eae52d5acc5598
Calum Leslie <calumleslie@gmail.com>
2016-11-16 13:27:14 +0000

    Reformat using cargo fmt.

--- src/messages/privmsg.rs
+++ src/messages/privmsg.rs
@@ -17,13 +17,15 @@ impl Message {
             return None;
         }
         if self.arguments.len() != 2 {
-            warn!("Not parsing message as Privmsg because we expect 2 arguments: {}", self);
+            warn!("Not parsing message as Privmsg because we expect 2 arguments: {}",
+                  self);
             return None;
         }
         let user = match self.prefix {
             Prefix::User(ref u) => u,
             _ => {
-                warn!("Not parsing user as Privmsg because we expect prefix of user: {}", self);
+                warn!("Not parsing user as Privmsg because we expect prefix of user: {}",
+                      self);
                 return None;
             }
         };
@@ -51,49 +53,50 @@ mod tests {
         let message = message(":nick!someone@somewhere PRIVMSG #channel :Hey everyone!\r\n");
         let privmsg = message.as_privmsg();
 
-        assert_eq!( privmsg, Some( Privmsg {
-            from: &UserInfo::of_nickname_user_host( "nick", "someone", "somewhere" ),
-            to: "#channel",
-            text: "Hey everyone!",
-        } ) );
+        assert_eq!(privmsg,
+                   Some(Privmsg {
+                       from: &UserInfo::of_nickname_user_host("nick", "someone", "somewhere"),
+                       to: "#channel",
+                       text: "Hey everyone!",
+                   }));
     }
 
     #[test]
     fn bad_no_message() {
         let message = message(":nick!someone@somewhere PRIVMSG #channel\r\n");
-        assert_eq!( message.as_privmsg(), None);
+        assert_eq!(message.as_privmsg(), None);
     }
 
     #[test]
     fn bad_too_many_arguments() {
         let message = message(":nick!someone@somewhere PRIVMSG #channel #anotherchannel \
                                :message\r\n");
-        assert_eq!( message.as_privmsg(), None);
+        assert_eq!(message.as_privmsg(), None);
     }
 
     #[test]
     fn bad_server_prefix() {
         let message = message(":test.irc.com PRIVMSG #channel :message\r\n");
-        assert_eq!( message.as_privmsg(), None);
+        assert_eq!(message.as_privmsg(), None);
     }
 
     #[test]
     fn bad_no_prefix() {
         let message = message("PRIVMSG #channel :message\r\n");
-        assert_eq!( message.as_privmsg(), None);
+        assert_eq!(message.as_privmsg(), None);
     }
 
     #[test]
     fn bad_not_privmsg() {
         let message = message(":nick!someone@somewhere PING #channel\r\n");
-        assert_eq!( message.as_privmsg(), None);
+        assert_eq!(message.as_privmsg(), None);
     }
 
     fn message(message: &str) -> Message {
         let parsed = Message::parse(message.as_bytes());
         match parsed {
             Ok((msg, _)) => msg,
-            other => panic!( "Could not parse {}, got result {:?}", message, other ),
+            other => panic!("Could not parse {}, got result {:?}", message, other),
         }
     }
 }
